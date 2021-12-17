4459de0f77c13419061e0bd898489e04415d7e56
Rianto <rianto.rnt@gmail.com>
2021-12-17 20:09:59 +0800

    Creating new angular component sales team table

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -10,10 +10,9 @@ export class SalesPersonListComponent implements OnInit {
   // Create an array of object
   salesPersonList: SalesPerson[] = [
     new SalesPerson('Ryan', 'Morrison', 'ryanmorrisonsay@yahoo.com', 50000),
-    new SalesPerson('Ryan', 'Morrison', 'ryanmorrisonsay@yahoo.com', 50000),
-    new SalesPerson('Ryan', 'Morrison', 'ryanmorrisonsay@yahoo.com', 50000),
-    new SalesPerson('Ryan', 'Morrison', 'ryanmorrisonsay@yahoo.com', 50000),
-    new SalesPerson('Ryan', 'Morrison', 'ryanmorrisonsay@yahoo.com', 50000),
+    new SalesPerson('jhon', 'doe', 'jon@yahoo.com', 40000),
+    new SalesPerson('R', 'R', 'rian@yahoo.com', 70000),
+    new SalesPerson('Rianto', 'RNT', 'rianto@yahoo.com', 60000),
   ];
 
   constructor() {}
